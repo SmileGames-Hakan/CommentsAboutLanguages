566963e083d62dce4d7dca341f658cf7db267bd8
SmileGames-Hakan <107798568+SmileGames-Hakan@users.noreply.github.com>
2022-06-19 16:37:34 +0300

    Add files via upload

---
+++ CommentAboutP.Languages.py
@@ -0,0 +1,23 @@
+#my comment about programing languages
+
+def DefineVariables():
+  global x
+  x = "beginner friend "
+  global y
+  y = "is so strong and hard"
+  global z
+  z = "bit hard"
+  global w
+  w = "best"
+  global t
+  t = "nice but hard to understand"
+  global c
+  c = "is sweet"
+
+DefineVariables()
+
+print("Python is " + x + "and " + w)
+print("C is " + y)
+print("Java is " + z)
+print("C++ is " + t)
+print("C# " + c)
